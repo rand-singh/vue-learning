8bbbd02e5f2920ec6f625d9139eb2482d08f6354
Randeep Singh <rand-singh@users.noreply.github.com>
2020-06-16 20:09:35 +0100

    learning vue - laracasts

---
+++ components/modal/main.js
@@ -0,0 +1,25 @@
+Vue.component("modal", {
+  props: ["info"],
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+  template: `
+      <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content box">
+          <slot></slot>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+      </div>
+  `,
+});
+
+new Vue({
+  el: "#root",
+
+  data: {
+    showModal: false,
+  },
+});
