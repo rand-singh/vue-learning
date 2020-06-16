43e5f3d9093aeb26d51db07aade22c79fee563e6
Randeep Singh <rand-singh@users.noreply.github.com>
2020-06-16 21:48:58 +0100

    Setup message component

---
+++ my-app/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box is-info">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.box{
+    background: grey;
+    padding: 10px;
+    border: 1px solid #c5c5c5;
+    margin-bottom: 1em;
+}
+
+</style>
